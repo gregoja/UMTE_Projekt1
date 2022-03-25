943e906971f9b76d25348d62473086b46a195242
Jan Gregovský <kkomtu@gmail.com>
2022-03-25 16:07:30 +0100

    Second Level partially done

--- app/src/main/java/com/example/umte_projekt1/MainActivity.java
+++ app/src/main/java/com/example/umte_projekt1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.umte_projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchFirstLevel(View view){
+        Intent intent = new Intent(this, FirstLevelActivity.class);
+        //intent.putExtra("message", editText.getText().toString());
+        startActivity(intent);
+    }
+
+    public void launchSecondLevel(View view){
+        Intent intent = new Intent(this, SecondLevelActivity.class);
+        //intent.putExtra("message", editText.getText().toString());
+        startActivity(intent);
+    }
+
+    public void launchThirdLevel(View view){
+        Intent intent = new Intent(this, ThirdLevelActivity.class);
+        //intent.putExtra("message", editText.getText().toString());
+        startActivity(intent);
+    }
+
+    public void launchFourthLevel(View view){
+        Intent intent = new Intent(this,FourthLevelActivity.class);
+        //intent.putExtra("message", editText.getText().toString());
+        startActivity(intent);
     }
 }
