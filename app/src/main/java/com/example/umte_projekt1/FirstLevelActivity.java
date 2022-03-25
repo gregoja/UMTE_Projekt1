8c83b8fe0848be1621af0aea8022df94cb0e507a
Jan Gregovský <kkomtu@gmail.com>
2022-03-25 19:00:14 +0100

    Third level partially done

--- app/src/main/java/com/example/umte_projekt1/FirstLevelActivity.java
+++ app/src/main/java/com/example/umte_projekt1/FirstLevelActivity.java
@@ -1,16 +1,50 @@
 package com.example.umte_projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import java.util.Objects;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class FirstLevelActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private final int PERMISSION_REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_level);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    public void requestCoordinates(View view){
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCoordinates();
+        } else {
+            // Požádáme o práva
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCoordinates() {
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) Toast.makeText(getApplicationContext(),"lat: " + location.getLatitude() + ", Lon: " + location.getLongitude(),Toast.LENGTH_SHORT).show();
+            else Toast.makeText(getApplicationContext(),"Poloha nezjištěna. Chyba! Chyba!",Toast.LENGTH_SHORT).show();
+        });
+    }
+
 }
--- app/src/main/java/com/example/umte_projekt1/ThirdLevelActivity.java
+++ app/src/main/java/com/example/umte_projekt1/ThirdLevelActivity.java
@@ -1,14 +1,48 @@
 package com.example.umte_projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ThirdLevelActivity extends AppCompatActivity {
+
+    private final int PERMISSION_REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_level);
+        requestCamera();
     }
+
+    public void launchQrActivity(View view){
+
+        StartQrActivity();
+    }
+
+    private void requestCamera() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // Práva máme, není potřeba o ně žádat
+
+        } else {
+            // Požádáme o práva
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+
+        }
+
+    }
+
+    private void StartQrActivity() {
+        Intent intent = new Intent(this, QrReaderActivity.class);
+        //intent.putExtra("message", editText.getText().toString());
+        startActivity(intent);
+    }
+
 }
